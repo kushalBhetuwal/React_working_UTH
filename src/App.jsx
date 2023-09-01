8f4a80ed8a23a66ae3642ff0f6951519643f63c1
kushal <kushalbhetuwal@gmail.com>
2023-09-02 00:02:31 +1000

    react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime:Infinity,
+      cacheTime:Infinity
+    }
+  }
+})
+
 const App = () => {
   return (
-    <div>
+   <div>
       <BrowserRouter>
+      <QueryClientProvider client = {queryClient}>
         <header>
           <Link to="/">Adopt Me!</Link>
         </header>
@@ -14,8 +25,9 @@ const App = () => {
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
         </Routes>
+        </QueryClientProvider>
       </BrowserRouter>
-    </div>
+      </div>
   );
 };
 
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,25 @@
-const Details =()=>{
-    return(
-        <div><h1>This is details page</h1></div>
-    )
-}
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+const Details = () => {
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+  const pet = results.data.pets[0];
+  return (
+    <div className="details">
+      <h1>{pet.name}</h1>
+      <h2>{pet.animal}—{pet.breed}—{pet.city}, {pet.state}</h2>
+      <button>Adopt{pet.name}</button>
+      <p>{pet.description}</p>
+    </div>
+  );
+};
 
 export default Details;
