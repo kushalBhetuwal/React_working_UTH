8c3dda3f81d955de40fb41c7ab77b93d8ff240a7
kushal <kushalbhetuwal@gmail.com>
2023-09-01 15:16:27 +1000

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { createRoot } from 'react-dom/client';
-import SearchParams from './SearchParams';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 
 const App = () => {
- return(
-  <div>
-    <h1>Adopt me</h1>
-    <SearchParams/>
-  </div>
- )
+  return (
+    <div>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App/>);
- 
+root.render(<App />);
---
+++ src/Details.jsx
@@ -0,0 +1,7 @@
+const Details =()=>{
+    return(
+        <div><h1>This is details page</h1></div>
+    )
+}
+
+export default Details;
