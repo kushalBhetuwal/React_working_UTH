9c5d1e3d04589a745566039ab1b7678fae3306b5
kushal <kushalbhetuwal@gmail.com>
2023-09-05 09:25:59 +1000

    usebreedlist modified

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,83 +1,85 @@
 import { useState, useEffect } from "react";
-import Results from './Results';
-import useBreedList from "./useBreedList";
-const animals = ["bird", "cat", "dog", "rabbit", "reptile"];
-
+import Pet from "./Pet";
+import useBreedList from './useBreedList';
+const Animals = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState(""); //this line always gives an array
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const [breeds]  = useBreedList(animal); 
+  const [breeds] = useBreedList(animal);
+
+  //const locationhook = useState('');
+  //const location = locationhook[0];
+  //const setLocation = locationhook[1];
+
+  //useEffect happens outside of the component
+  
+
   useEffect(() => {
     requestPets();
-    
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   async function requestPets() {
-    const response = await fetch(
+    const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
-    const json = await response.json();
-    console.log(json)
+    const json = await res.json();
     setPets(json.pets);
   }
+
   return (
     <div className="search-params">
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          requestPets();
+          requestPets(); 
         }}
       >
-        <label htmlFor="location">
-          Location
-          <input
-            id="location"
-            onChange={(e) => setLocation(e.target.value)}
-            value={location}
-            placeholder="Location"
-          />
-        </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            disabled={animals.length === 0}
-            value={animal}
-            onChange={(e) => {
-              setAnimal(e.target.value);
-              setBreed("");
-            }}
-            onBlur={(e) => {
-              setAnimal(e.target.value);
-              setBreed("");
-            }}
-          >
-            <option />
-            {animals.map((animal) => (
-              <option key={animal}>{animal}</option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            disabled={breeds.length === 0}
-            onChange={(e) => setBreed(e.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-            value={breed}
-            placeholder="Breed"
-          >
-            <option />
-            {breeds.map((breed) => (
-              <option key={breed}>{breed}</option>
-            ))}
-          </select>
-        </label>
+        <label htmlFor="search">Search</label>
+        <input
+          id="location"
+          onChange={(e) => setLocation(e.target.value)}
+          value={location}
+          placeholder="Location"
+        />
+        <label htmlFor="animal">Animal</label>
+        <select
+          id="animal"
+          onChange={(e) => {
+            setAnimal(e.target.value);
+            setBreed("");
+          }}
+          value={animal}
+        >
+          <option />
+          {Animals.map((animal) => (
+            <option key={animal}>{animal}</option>
+          ))}
+        </select>
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          disabled={breeds.length === 0}
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+        >
+          <option />
+          {breeds.map((breed) => (
+            <option key={breed}>{breed}</option>
+          ))}
+        </select>
+
         <button>Submit</button>
       </form>
-      <Results pets ={pets}/>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
--- src/useBreedList.jsx
+++ src/useBreedList.jsx
@@ -1,34 +1,32 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 const localcache = {};
 
-export default function useBreedList(animal){
-    const [breedlist, setBreedList] = useState([]);
-    const [status, setStatus] = useState('unloaded');
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
 
-    useEffect(()=>{
-        if(!animal){
-            setBreedList([]);
-        }
-        else if(localcache[animal]){
-            setBreedList(localcache[animal])
-        }
-        else{
-            requestBreedList();
-        }
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localcache[animal]) {
+      setBreedList(localcache[animal]);
+    } else {
+      requestBreedList();
+    }
 
-        async function  requestBreedList(){
-            setStatus('loading');
-            setBreedList([]);
-            const res = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
-            const json = await res.json();
-            localcache[animal] = json.breeds || [];
-            setBreedList(localcache[animal]);
-            setStatus("loaded");
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localcache[animal] = json.breeds || [];
+      setBreedList(localcache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
 
-        }
-    }, [animal])
-
-    return [breedlist, status];
- 
+  return [breedList, status];
 }
