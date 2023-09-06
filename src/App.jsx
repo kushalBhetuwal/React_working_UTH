ca77df641eb88263705c7e6ac1b93d6315fb27e3
kushal <kushalbhetuwal@gmail.com>
2023-09-06 11:52:26 +1000

    added queryclient

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,21 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime:Infinity,
+      cacheTime:Infinity
+    }
+  }
+})
 
 const App = () => {
   return (
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <div>
       <header>
         <Link to ="/"><h1>Adopt me!</h1></Link>
@@ -16,6 +27,8 @@ const App = () => {
         <Route path = "/" element = {<SearchParams/>}/>
         </Routes>
       </div>
+    </QueryClientProvider>
+    
     </BrowserRouter>
   );
 };
